06babbbe0db83d4c24e293ef8b045b8c1a3c15e0
daichi <daipanchi@gmail.com>
2015-09-01 23:50:30 +0900

    Initial Commit

---
+++ reply_on_notification/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  reply_on_notification
+//
+//  Created by dch on 9/1/15.
+//  Copyright (c) 2015 co.l0o0l. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
